227224134168fcb89f60b852aad71c86f1916050
qoltiboyev <qamariddinoltiboyevdev@gmail.com>
2023-11-22 18:49:15 +0500

    k sonni n marta chiqaruvchi dastur

---
+++ for1.dart
@@ -0,0 +1,12 @@
+void main() {
+  int k = 5;
+  int n = 1;
+
+  if (n > 0) {
+    for (int i = 0; i < n; i++) {
+      print(k);
+    }
+  } else {
+    print("Xatolik: n>0 shart bajarilmaydi");
+  }
+}
